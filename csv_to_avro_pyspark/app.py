926e94e2a0dfcc8c51f3429ebb6920cf99e759fc
Bruno Luz <bruno.al.luz@gmail.com>
2021-02-15 16:10:58 -0300

    pyspark csv to avro

--- csv_to_avro_pyspark/app.py
+++ csv_to_avro_pyspark/app.py
@@ -1,29 +1,23 @@
 import pyspark
 import pathlib
 import os
+
 from pyspark.sql import SparkSession
 import shutil
 
-
 current_dir = pathlib.Path().absolute()
 sc = pyspark.SparkContext('local[*]')
-spark = SparkSession(sc)
+spark = SparkSession(sc).builder.getOrCreate()
 spark.conf.set("spark.sql.avro.compression.codec", "deflate")
 
 
 def execute(file):
-
-    out_dir = os.path.join(current_dir, 'data_samples', 'bruno')
+    out_dir = os.path.join(current_dir, 'data_samples', 'output')
     if os.path.isdir(out_dir):
         shutil.rmtree(out_dir)
 
-    # rdd = sc.textFile(file) \
-    #    .map(lambda line: line.split(";"))
-
-    rdd = sc.parallelize([{'id': 1}, {'id': 2}, {'id': 3}])
-
-    df = rdd.toDF()
-    # df.coalesce(1).write.format("avro").save(out_dir)
+    rdd = spark.read.format('csv').options(header='true', inferSchema='true').load(file)
+    df = getattr(rdd, 'toDF')(*rdd.columns)
     df.write.format("avro").save(out_dir)
 
 
--- csv_to_avro_pyspark/app2.py
+++ csv_to_avro_pyspark/app2.py
@@ -6,7 +6,7 @@ from pyspark.sql import SparkSession
 
 if __name__ == '__main__':
     current_dir = pathlib.Path().absolute()
-    out_dir = os.path.join(current_dir, 'data_samples', 'bruno')
+    out_dir = os.path.join(current_dir, 'data_samples', 'output')
     sc = pyspark.SparkContext('local[*]')
     spark = SparkSession(sc)
     spark.read.format('avro').load(out_dir).show()
