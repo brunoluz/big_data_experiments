77af9ba759a4b127a0a60ca69a0774e1edc76bde
Bruno Luz <bruno.al.luz@gmail.com>
2021-02-15 13:37:22 -0300

    pyspark

--- csv_to_avro_pyspark/app.py
+++ csv_to_avro_pyspark/app.py
@@ -1,18 +1,32 @@
+import json
+
 import pyspark
 import pathlib
 import os
+from pyspark.sql import SparkSession
 
 current_dir = pathlib.Path().absolute()
+sc = pyspark.SparkContext('local[*]')
+spark = SparkSession(sc)
 
 
-def convert_csv_to_avro():
-    sc = pyspark.SparkContext('local[*]')
+def execute(file):
 
-    csv_file = os.path.join(current_dir, 'dataset.csv')
+    out_dir = os.path.join(current_dir, 'data_samples', 'bruno')
 
-    txt = sc.textFile(csv_file)
-    print(txt.count())
+    rdd = sc.textFile(file) \
+        .map(lambda line: line.split(";"))
+
+    df = rdd.toDF()
+    df.write.save(out_dir)
+
+
+
+    # rdd.saveAsTextFile(os.path.join(current_dir, 'data_samples', 'bruno'))
+
+    # rdd.saveAsHadoopFile(os.path.join(current_dir, 'data_samples', 'bruno'))
+    # json_rdd.saveAsNewAPIHadoopFile(os.path.join(current_dir, 'data_samples', 'bruno'), "org.apache.avro.mapreduce.AvroKeyOutputFormat")
 
 
 if __name__ == '__main__':
-    convert_csv_to_avro()
+    execute(os.path.join(current_dir, 'data_samples', 'dataset.csv'))
