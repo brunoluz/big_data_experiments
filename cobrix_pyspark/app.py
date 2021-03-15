173ec6ee7fd812d95543b8db71496a3f2de38b96
Bruno Luz <bruno.al.luz@gmail.com>
2021-03-15 00:05:13 -0300

    cobrix on windows

--- cobrix_pyspark/app.py
+++ cobrix_pyspark/app.py
@@ -11,8 +11,19 @@ data_dir = os.path.join(current_dir, 'data_samples')
 copybook_file = os.path.join(data_dir, 'test1_copybook.cob')
 ebcdic_file = os.path.join(data_dir, 'test1_data.bin')
 
+if os.name == 'nt':
+    copybook_file = 'file:\\' + copybook_file
+    ebcdic_file = 'file:\\' + ebcdic_file
+else:
+    copybook_file = 'file://' + copybook_file
+    ebcdic_file = 'file://' + copybook_file
+
 sc = pyspark.SparkContext('local[*]')
 spark = SparkSession(sc)
 
-df = spark.read.format('cobol').option("copybook", 'file://' + copybook_file).load('file://' + ebcdic_file)
+df = spark.read\
+    .format('cobol')\
+    .option("copybook", copybook_file)\
+    .load(ebcdic_file)
+
 df.show()
