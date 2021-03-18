52d0587657a1ae3d24ea1d3167196a109d92baf0
Bruno Luz <bruno.al.luz@gmail.com>
2021-03-18 20:27:55 -0300

    cobrix 2

--- cobrix_pyspark/app.py
+++ cobrix_pyspark/app.py
@@ -1,29 +1,37 @@
 import os
 import pathlib
 import pyspark
+from datetime import datetime
 from pyspark.sql import SparkSession
-
+import json
 
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages za.co.absa.cobrix:spark-cobol_2.12:2.2.1 pyspark-shell'
 
 current_dir = pathlib.Path().absolute()
-data_dir = os.path.join(current_dir, 'data_samples')
-copybook_file = os.path.join(data_dir, 'test1_copybook.cob')
-ebcdic_file = os.path.join(data_dir, 'test1_data.bin')
+copybook_file = os.path.join(current_dir, 'data_samples', 'test1_copybook.cob')
+ebcdic_file = os.path.join(current_dir, 'data_samples', 'test1_data.bin')
+output_file = os.path.join(current_dir, 'output', f"{datetime.now().strftime('%Y%m%d_%H%M%S')}.json")
 
-if os.name == 'nt':
+# tratativa para spark usar arquivos locais em vez do HDFS
+if os.name == 'nt':  # Windows
     copybook_file = 'file:\\' + copybook_file
     ebcdic_file = 'file:\\' + ebcdic_file
-else:
+else:  # Linux
     copybook_file = 'file://' + copybook_file
     ebcdic_file = 'file://' + copybook_file
 
 sc = pyspark.SparkContext('local[*]')
 spark = SparkSession(sc)
 
-df = spark.read\
-    .format('cobol')\
-    .option("copybook", copybook_file)\
+df = spark.read \
+    .format('cobol') \
+    .option("copybook", copybook_file) \
     .load(ebcdic_file)
 
-df.show()
+items = df.toJSON().collect()
+result = []
+for i in items:
+    result.append(json.loads(i))
+
+with open(output_file, 'w') as outfile:
+    json.dump(result, outfile, indent=3)
